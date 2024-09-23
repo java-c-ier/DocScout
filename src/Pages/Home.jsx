983c90c9729861ba6fbf4aaaae6153ba8366f286
java-c-ier <jimutsahoo02@gmail.com>
2024-09-23 10:08:19 +0530

    Imports removed.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import Hero from './../Components/Hero';
-import About from './About';
-import Testimonial from './Testimonial';
-import Contact from './../Components/Contact';
 
 function Home() {
   return (

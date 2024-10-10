15e2d67ea3d909f1bf6c2013094d6449634acb3f
java-c-ier <jimutsahoo02@gmail.com>
2024-10-10 17:46:48 +0530

    New route to file upload added.

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
-import Testimonial from "./../Pages/Testimonial";
-import Contact from "./../Components/Contact";
+import Testimonial from "../Pages/Testimonial";
+import Contact from "../Components/Contact";
 import SignIn from "../Pages/SignIn";
 import SignUp from "../Pages/SignUp";
 import Nav from "../Components/Nav";
+import CSVUpload from '../Components/CSVUpload';
 
 function Routing() {
   return (
     <div>
-      <Nav></Nav>
+      <Nav />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/testimonials" element={<Testimonial />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/signin" element={<SignIn />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/testimonials" element={<Testimonial />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/upload" element={<CSVUpload />} />
       </Routes>
     </div>
   );

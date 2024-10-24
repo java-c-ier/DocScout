edf6349e219926a76105718b0e067353d9017d69
java-c-ier <jimutsahoo02@gmail.com>
2024-10-24 13:54:28 +0530

    Moved to different location.

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className='w-full px-12 my-1 font-semibold text-xl' id="contact">Contact</div>
+  )
+}
+
+export default Contact

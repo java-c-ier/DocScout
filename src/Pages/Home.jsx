b9e0ec355d0431811356f7865a91dc091143ac50
java-c-ier <jimutsahoo02@gmail.com>
2024-10-24 21:17:08 +0530

    Added footer emmet.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from './../Components/Hero';
 import About from './About';
 import Testimonial from './Testimonial';
 import Contact from './Contact';
+import { Footer } from './Footer';
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <About></About>
       <Testimonial></Testimonial>
       <Contact></Contact>
+      <Footer></Footer>
     </div>
   )
 }

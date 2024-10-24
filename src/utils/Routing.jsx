db42fd30e359225ec4a768b0441b6523dae0653a
java-c-ier <jimutsahoo02@gmail.com>
2024-10-24 13:51:33 +0530

    Deleted not necessary imports.

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
-import About from "../Pages/About";
-import Testimonial from "../Pages/Testimonial";
-import Contact from "../Components/Contact";
 import SignIn from "../Pages/SignIn";
 import SignUp from "../Pages/SignUp";
 import Nav from "../Components/Nav";
-import CSVUpload from '../Components/CSVUpload';
+import CSVUpload from "../Components/CSVUpload";
 
 function Routing() {
   return (
@@ -15,12 +12,9 @@ function Routing() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/testimonials" element={<Testimonial />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/upload" element={<CSVUpload />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/upload" element={<CSVUpload />} />
       </Routes>
     </div>
   );

1cb7918e0f1874171c86203d8ca687a5bfc4ff2c
java-c-ier <jimutsahoo02@gmail.com>
2024-10-24 21:16:50 +0530

    Contact form for any query.

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,9 +1,176 @@
-import React from 'react'
+import React from "react";
+import { Button, Input, Textarea, Typography } from "@material-tailwind/react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function Contact() {
+export function Contact() {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "0e132523-6194-4c0b-8dd8-c875796fbdec");
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        toast.success("Message sent successfully.");
+        event.target.reset();
+      } else {
+        setResult("Failed to submit");
+        toast.error("Error! Please try again later.");
+      }
+    } catch (error) {
+      setResult("Network error");
+      toast.error("Network error! Please try again later.");
+    }
+  };
+
   return (
-    <div className='w-full px-12 my-1 font-semibold text-xl' id="contact">Contact</div>
-  )
+    <section className="px-8 pt-16" id="contact">
+      <div className="container mx-auto text-center">
+        <Typography
+          variant="h1"
+          color="blue-gray"
+          className="mb-4 !text-3xl lg:!text-5xl"
+        >
+          We&apos;re Here to Help
+        </Typography>
+        <Typography className="mb-10 font-normal !text-lg lg:mb-12 mx-auto max-w-3xl text-gray-800">
+          Whether it&apos;s a question about our services, a request for
+          technical assistance, or suggestions for improvement, our team is
+          eager to hear from you.
+        </Typography>
+        <div className="grid grid-cols-1 gap-x-12 gap-y-6 lg:grid-cols-2 items-start justify-items-center">
+          <img
+            src="/src/assets/customer.png"
+            alt="map"
+            className="h-full lg:max-h-[510px] rounded-md"
+          />
+          <form
+            onSubmit={onSubmit}
+            action="#"
+            className="flex flex-col gap-4 lg:max-w-sm"
+          >
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <Typography
+                  variant="small"
+                  className="mb-2 text-left font-medium !text-gray-900"
+                >
+                  First Name
+                </Typography>
+                <Input
+                  required
+                  color="black"
+                  size="lg"
+                  placeholder="First Name"
+                  name="first-name"
+                  className="focus:border-t-gray-900"
+                  containerProps={{
+                    className: "!min-w-full",
+                  }}
+                  labelProps={{
+                    className: "hidden",
+                  }}
+                />
+              </div>
+              <div>
+                <Typography
+                  variant="small"
+                  className="mb-2 text-left font-medium !text-gray-900"
+                >
+                  Last Name
+                </Typography>
+                <Input
+                  required
+                  color="gray"
+                  size="lg"
+                  placeholder="Last Name"
+                  name="last-name"
+                  className="focus:border-t-gray-900"
+                  containerProps={{
+                    className: "!min-w-full",
+                  }}
+                  labelProps={{
+                    className: "hidden",
+                  }}
+                />
+              </div>
+            </div>
+            <div>
+              <Typography
+                variant="small"
+                className="mb-2 text-left font-medium !text-gray-900"
+              >
+                Your Email
+              </Typography>
+              <Input
+                required
+                color="gray"
+                size="lg"
+                placeholder="name@email.com"
+                name="email"
+                className="focus:border-t-gray-900"
+                containerProps={{
+                  className: "!min-w-full",
+                }}
+                labelProps={{
+                  className: "hidden",
+                }}
+              />
+            </div>
+            <div>
+              <Typography
+                variant="small"
+                className="mb-2 text-left font-medium !text-gray-900"
+              >
+                Your Message
+              </Typography>
+              <Textarea
+                required
+                rows={6}
+                color="gray"
+                placeholder="Message"
+                name="message"
+                className="focus:border-t-gray-900"
+                containerProps={{
+                  className: "!min-w-full",
+                }}
+                labelProps={{
+                  className: "hidden",
+                }}
+              />
+            </div>
+            <Button type="submit" className="w-full" color="blue">
+              Send message
+            </Button>
+            <ToastContainer
+              position="top-center"
+              autoClose={4000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="colored"
+            />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;

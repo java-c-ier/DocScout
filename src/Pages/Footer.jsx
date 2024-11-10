1a2204f4829bf08b793c8737bca9009ddacaa00f
java-c-ier <jimutsahoo02@gmail.com>
2024-11-10 21:14:42 +0530

    Copyright name changed.

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -11,7 +11,7 @@ export function Footer() {
             variant="small"
             className="mb-4 text-center font-normal text-md text-blue-gray-900 md:mb-0"
           >
-            &copy; {currentYear} Jimut. All Rights Reserved.
+            &copy; {currentYear} DocScout. All Rights Reserved.
           </Typography>
           <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
             <Typography

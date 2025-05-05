af83b46b0ae427adb9d0d3c408925a087cd0edc8
java-c-ier <jimutsahoo02@gmail.com>
2025-05-06 02:11:42 +0530

    Added routes for doctors' info upload.

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
 import SignIn from "../Pages/SignIn";
 import SignUp from "../Pages/SignUp";
 import Nav from "../Components/Nav";
 import CSVUpload from "../Components/CSVUpload";
+import CSVUploadDoctors from "../Components/CSVUploadDoctors";
 
 function Routing() {
   return (
@@ -13,6 +14,7 @@ function Routing() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/upload" element={<CSVUpload />} />
+        <Route path="/uploadDoctors" element={<CSVUploadDoctors />} />  
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>

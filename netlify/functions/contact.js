41e60140b81f607aa39f056db087c140fdc57d10
Jimut <jimut_ks@trisysit.com>
2026-07-14 09:15:55 +0530

    Use verified jimut.in domain for sending emails via Resend

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -1,6 +1,6 @@
 const RESEND_API_KEY = process.env.RESEND_API_KEY;
 const TO_EMAIL = "jimutksahoo99@gmail.com";
-const FROM_EMAIL = "onboarding@resend.dev";
+const FROM_EMAIL = "doc-scout@jimut.in";
 
 exports.handler = async (event) => {
   if (event.httpMethod !== "POST") {
